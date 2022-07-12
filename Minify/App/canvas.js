7d583ee68d3d8fd5742c87163feb79440c8c3e24
mobin-ghaemi <mobinghaemi@yahoo.com>
2022-07-12 12:26:45 +0430

    Optimization , Fix Bugs

--- Minify/App/canvas.js
+++
@@ -1,5 +0,0 @@
-const rectVolumeY=10,rectVolumeX=13,windowShade=document.querySelector("canvas#windowShade"),WS_CTX=windowShade.getContext("2d");WS_CTX.fillStyle="#CED9E3",WS_CTX.fillRect(13,10,4,22),WS_CTX.fillRect(13,40,4,22),WS_CTX.fillRect(13,70,4,22),WS_CTX.fillRect(13,100,4,22),WS_CTX.fillRect(13,130,4,22),WS_CTX.fillRect(13,160,4,22),WS_CTX.fillRect(13,190,4,22),
-//! Create Circle of the end window shade
-WS_CTX.beginPath(),WS_CTX.arc(15,230,13,0,2*Math.PI,!0),WS_CTX.fillStyle="#1CB0F688",WS_CTX.fill(),
-//! Create Circle of the end window shade
-WS_CTX.beginPath(),WS_CTX.arc(15,230,9,0,2*Math.PI,!0),WS_CTX.fillStyle="#1CB0F6",WS_CTX.fill();
--- readme.md
+++
@@ -1,5 +0,0 @@
-name : mobin
-
-This is a Persian Website ,
-searching and showing Persian meaning!
-my name is mobin
